b8a5fd5e18ad022a4678931721fb8881859da174
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-03-02 16:43:33 +0530

    Create composite.c

---
+++ composite.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a,i,count=0;
+	scanf("%d",&a);
+	for(i=1;i<=a;i++)
+	{
+		if(a%i==0)
+		count++;
+	}
+	if(count>2)
+	printf("%d is the composite number",a);
+	else
+	printf("%d is not composite number",a);
+	return 0;
+}

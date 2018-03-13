0c37539b8d97669ddcd2f709aaafc8f3de60e4d8
ananthkgm <35257712+ananthkgm@users.noreply.github.com>
2018-03-13 16:13:20 +0530

    Create min10.c

---
+++ min10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+	int a[10],i,smallest;
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	smallest=a[0];
+	for(i=0;i<10;i++)
+	{
+		if(a[i]<smallest)
+		smallest=a[i];
+	}
+	printf("%d",smallest);
+}
